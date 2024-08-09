977e2f0d7d23ac8b25a52a4ac65948c791a5c008
vuan <duyanvu2409@gmail.com>
2024-08-10 00:01:18 +0700

    first commit

---
+++ SIMS_SE06205/Controllers/StudentController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SIMS_SE06205.Models;
+
+namespace SIMS_SE06205.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string filePathStudent = @"D:\DDD\APDP-BTec-main\data-sims\\data-student.json";
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string dataJson = System.IO.File.ReadAllText(filePathStudent);
+            StudentModel model = new StudentModel();
+            model.StudentLists = new List<StudentViewModel>();
+            var students = JsonConvert.DeserializeObject<List<StudentViewModel>>(dataJson);
+            var dataStudents = (from s in students select s).ToList();
+            foreach (var item in dataStudents)
+            {
+                model.StudentLists.Add(new StudentViewModel
+                {
+                    Id = item.Id,
+                    Code = item.Code,
+                    FirstName = item.FirstName,
+                    LastName = item.LastName,
+                    Email = item.Email,
+                    Address = item.Address,
+                    Phone = item.Phone,
+                    Gender = item.Gender,
+                    BirthDay = item.BirthDay,
+
+                });
+
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            StudentViewModel model = new StudentViewModel();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(StudentViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //bao loi
+                try
+                {
+                    string dataJson = System.IO.File.ReadAllText(filePathStudent);
+                    var students = JsonConvert.DeserializeObject<List<StudentViewModel>>(dataJson);
+                    int maxId = 0;
+                    if (students != null)
+                    {
+                        maxId = int.Parse((from s in students select s.Id).Max()) + 1;
+                    }
+                    string idIncrement = maxId.ToString();
+                    students.Add(new StudentViewModel
+                    {
+                        Id = idIncrement,
+                        Code = model.Code,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email,
+                    Address = model.Address,
+                        
+                    Phone = model.Phone,
+                    Gender = model.Gender,
+                        
+                    BirthDay = model.BirthDay,
+
+                    });
+                    var dtJson = JsonConvert.SerializeObject(students, Formatting.Indented);
+                    System.IO.File.WriteAllText(filePathStudent, dtJson);
+                    TempData["saveStatus"] = true; 
+                }
+
+                catch
+                {
+                    TempData["saveStatus"] = false;
+                }//quay ve trang list
+                return RedirectToAction(nameof(StudentController.Index), "Student");
+            }
+            return View(model);
+        }
+        //phan nay bat dau lam xoa
+     
+
+    }
+}
---
+++ SIMS_SE06205/Models/StudentViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using Newtonsoft.Json;
+
+namespace SIMS_SE06205.Models
+{
+    public class StudentModel
+    {
+        public List<StudentViewModel> StudentLists { get; set; }
+    }
+    public class StudentViewModel
+    {
+        [Key]
+        public string? Id { get; set; }
+        [Required(ErrorMessage = "First Name's can be not emty")]
+
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name can be not emty")]
+
+        public string LastName { get; set; }
+       
+        public string Code {  get; set; }
+
+        [Required(ErrorMessage = "Email's Student can be not emty")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Phone's Student can be not emty")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Address's Student can be not emty")]
+        public string Address { get; set; }
+       
+        [AllowNull]
+        public string? Gender { get; set; }
+        [AllowNull]
+        public string? BirthDay { get; set; }
+
+
+
+    }
+}
